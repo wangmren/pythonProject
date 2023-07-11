267c0e9af33ac1da78307a5af501a586b4b3a4be
why <1966652060@qq.com>
2023-07-11 11:16:06 +0800

    使用get获取内容

--- Baidu/Baidu/spiders/baidu.py
+++ Baidu/Baidu/spiders/baidu.py
@@ -7,7 +7,8 @@ class BaiduSpider(scrapy.Spider):
     start_urls = ["https://www.baidu.com"]
 
     def parse(self, response):
-        result = response.xpath('/html/head/title/text()')[0]
+        # result = response.xpath('/html/head/title/text()')[0]
+        result = response.xpath('/html/head/title/text()').get()
         print('*'*50)
         print(result)
         print('*'*50)
