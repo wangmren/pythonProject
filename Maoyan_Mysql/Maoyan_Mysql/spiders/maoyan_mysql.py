dbf858de18fd6bb5af534c60e9195e2a943efe17
why <1966652060@qq.com>
2023-07-12 12:19:36 +0800

    多线程爬取

--- Maoyan_Mysql/Maoyan_Mysql/spiders/maoyan_mysql.py
+++ Maoyan_Mysql/Maoyan_Mysql/spiders/maoyan_mysql.py
@@ -4,17 +4,23 @@ from ..items import MaoyanMysqlItem
 class MaoyanMysqlSpider(scrapy.Spider):
     name = "maoyan_mysql"
     allowed_domains = ["maoyan.com"]
-    start_urls = ["https://maoyan.com/board/4?offset=0"]
-    offset = 0
+    start_urls = ["https://maoyan.com"]
+
+    # 重写父类的方法
+    def start_requests(self):
+        for offset in range(0, 91, 10):
+            url = 'https://www.maoyan.com/board/4?timeStamp=1638962905523&channelId=40011&index=1&signKey=cb357464ef369578b9d3d41970526d35&sVersion=1&webdriver=false&offset={}'.format(
+                offset)
+            # 交给调度器入队列
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        print(response)  # 为了防止校验
-
+        print(response)
         # 加载items对象
         item = MaoyanMysqlItem()
-        # 基础的xpath
+        # 获取电影列表
         dd_list = response.xpath('//dl[@class="board-wrapper"]/dd')
-        # 依次遍历
+        # 遍历list 解析
         for dd in dd_list:
             item['name'] = dd.xpath('./a/@title').get().strip()
             item['star'] = dd.xpath('.//p[@class="star"]/text()').get().strip()
@@ -23,15 +29,6 @@ class MaoyanMysqlSpider(scrapy.Spider):
             # item['time']=(len(time)<10 and time+'-01' or time)#三目的写法 只能添加一个 - 01
             item['time'] = self.zhengli(time)
             yield item
-        self.offset += 10
-        if self.offset <= 91:
-            url = 'https://maoyan.com/board/4?offset={}'.format(self.offset)
-            # 交给调度器进入队列
-            yield scrapy.Request(
-                url=url,
-                # 指定解析函数对象
-                callback=self.parse
-            )
 
     def zhengli(self, time):
         if len(time) < 10:
